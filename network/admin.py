40ce30676f0f3e0e6b6e176ad5e749ced7b85b82
bitTorin <torin.miner@gmail.com>
2021-12-16 15:56:29 -0600

    updated models

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import User, Post
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "body")
+
+admin.site.register(User)
+admin.site.register(Post, PostAdmin)
--- network/models.py
+++ network/models.py
@@ -1,6 +1,29 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    body = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.title}"
+
+class Like(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='like_user')
+    posts = models.ManyToManyField(Post, blank=True)
+
+    def __str__(self):
+        return f"{self.user}: {self.posts}"
+
+class Followers(models.Model):
+    account = models.ForeignKey('User', on_delete=models.CASCADE, related_name='account')
+    follower = models.ForeignKey('User', on_delete=models.CASCADE, related_name='follower')
+
+    def __str__(self):
+        return f"{self.account}: {self.follower}"
