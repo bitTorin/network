970b9969cddf1cf691a7b47f0c561ff533a8113a
bitTorin <torin.miner@gmail.com>
2021-12-18 16:03:23 -0600

    edited models

--- network/models.py
+++ network/models.py
@@ -10,20 +10,22 @@ class User(AbstractUser):
 class Post(models.Model):
     title = models.CharField(max_length=64)
     body = models.CharField(max_length=500)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='post_user')
 
     def __str__(self):
         return f"{self.title}"
 
 class Like(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='like_user')
-    posts = models.ManyToManyField(Post, blank=True)
+    post = models.ForeignKey('User', on_delete=models.CASCADE, related_name='likes')
 
     def __str__(self):
-        return f"{self.user}: {self.posts}"
+        return f"{self.user}: {self.post}"
 
 class Followers(models.Model):
     account = models.ForeignKey('User', on_delete=models.CASCADE, related_name='account')
-    follower = models.ForeignKey('User', on_delete=models.CASCADE, related_name='follower')
+    follower = models.ForeignKey('User', on_delete=models.CASCADE, related_name='followers')
 
     def __str__(self):
         return f"{self.account}: {self.follower}"
