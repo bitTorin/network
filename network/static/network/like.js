fb45c18185931c9ecbf8924e44b15dacb3c23fce
bitTorin <torin.miner@gmail.com>
2021-12-18 16:09:17 -0600

    created file, added placeholder 'like' function

---
+++ network/static/network/like.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#like-post').addEventListener('click', like_post);
+});
+
+function like_post() {
+    // Save post variables
+    const title = document.querySelector('#post-title').value;
+   
+    console.log(recipients);
+
+    // Pass info to API
+    fetch('/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            // TODO
+        })
+      })
+      .then(response => response.json())
+      
+      .then(result => {
+        // Print result
+        console.log(result);
+      })
+    
+      // Re-load likes TODO - FUNCTION NEEDED
+      .then(load_likes('post'))
+      
+      // Catch errors
+      .catch(error => {
+        console.log('Error:', error);
+      });
+    
+      return false;
+}
